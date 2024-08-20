ed8bae03d0fd8176e9c53e1eb2ef0504fe1da092
TinaL <tina.tsaiting@gmail.com>
2024-08-20 11:20:13 +0800

    feat: 調整範例內容

--- src/main/kotlin/coroutine/Coroutine.kt
+++ src/main/kotlin/coroutine/Coroutine.kt
@@ -5,7 +5,9 @@ import kotlinx.coroutines.flow.*
 import kotlin.system.measureTimeMillis
 
 fun main(args: Array<String>) {
-    stateFlowTest()
+    runBlocking {
+        executeThread()
+    }
 }
 
 // 1. thread 和 coroutine
@@ -35,10 +37,8 @@ private fun executeCoroutine() {
     println("thread: $coroutineExecuteTime")
 }
 
-
-
 // 2. launch / coroutineScope / async / join
-// runBlocking - new 一個新的coroutine，並block住當前的thread，把內部程式碼執行完才算結束
+// runBlocking - 建立一個新的coroutine scope，並block住當前的thread，把內部程式碼執行完才算結束
 // launch - 建立子coroutine，回傳Job，沒有回傳值，執行完就結束了
 private fun launchTest() {
     runBlocking {
@@ -57,22 +57,33 @@ private fun launchTest() {
     println("Done")
 }
 
-// coroutineScope - 分隔程式區塊，不會建立新的coroutine，繼承外面的coroutine
-private fun coroutineScopeTest() {
-    runBlocking {
-        coroutineScope {
+// coroutineScope - 在現有 suspend fun 中建立 scope,內部的 coroutine 都執行完才會繼續執行下面的程式碼
+private suspend fun coroutineScopeTest() {
+    coroutineScope {
+        launch {
             println("launch 1 start")
             delay(1000)
             println("launch 1 end")
         }
-        coroutineScope {
+        launch {
             println("launch 2 start")
             delay(500)
             println("launch 2 end")
         }
-//        println("Done $data1 , $data2")
-        println("Done")
     }
+    coroutineScope {
+        launch {
+            println("launch 3 start")
+            delay(1000)
+            println("launch 3 end")
+        }
+        launch {
+            println("launch 4 start")
+            delay(500)
+            println("launch 4 end")
+        }
+    }
+    println("Done")
 }
 
 private fun asyncTest() {
@@ -91,23 +102,19 @@ private fun asyncTest() {
 
 private fun joinTest() {
     runBlocking {
-        // 建立子coroutine，回傳deferred，等於有回傳值的Job
         val job = launch {
             println("launch start")
             delay(500)
             println("launch end")
             2
         }
-//        job.join()
+        job.join()// 等待job執行結束才會執行下面的程式碼
         println("Done")
     }
 }
 
-
-
-
-// 3. sharedFlow / stateFlow
-fun sharedFlowTest() = runBlocking {
+// 3. sharedFlow / stateFlow / combine / shareIn / stateIn
+suspend fun sharedFlowTest() = coroutineScope {
     val dataUseCase = DataUseCase()
     val sharedFlow = dataUseCase.getSharedFlow()
     launch {
@@ -124,15 +131,14 @@ fun sharedFlowTest() = runBlocking {
     println("done")
 }
 
-fun stateFlowTest() = runBlocking {
+suspend fun stateFlowTest() = coroutineScope {
     val dataUseCase = DataUseCase()
-    val stateFlow = dataUseCase.stateFlow()
+    val stateFlow = dataUseCase.getStateFlow()
     launch {
         stateFlow.collect {
             println("stateFlow (1): $it")
         }
     }
-//    yield()
     launch {
         delay(2000)
         stateFlow.collect {
@@ -143,7 +149,10 @@ fun stateFlowTest() = runBlocking {
 }
 
 class DataUseCase {
-    val scope = CoroutineScope(Job())
+    val scope = CoroutineScope(SupervisorJob())
+    private val sharedFlow = MutableSharedFlow<String>(replay = 1)
+    private val stateFlow = MutableStateFlow<String>("")
+
     suspend fun getSharedFlow(): Flow<Int> = flow {
         println("shared Flow started")
         repeat(6) {
@@ -152,12 +161,11 @@ class DataUseCase {
         }
     }.shareIn(
         scope,
-        replay = 2,
-        started = SharingStarted.WhileSubscribed()
+        replay = 1,
+        started = SharingStarted.Eagerly
     )
 
-
-    suspend fun stateFlow(): Flow<Int> = flow {
+    suspend fun getStateFlow(): Flow<Int> = flow {
         println("state Flow started")
         repeat(8) {
             delay(500)
@@ -166,7 +174,21 @@ class DataUseCase {
     }.stateIn(
         scope
     )
+
+
+    suspend fun combine(): Flow<Int> =
+        combine(getSharedFlow(), getStateFlow()) { a, b ->
+            println("$a, $b")
+            a + b
+        }
+
+
 }
+/**
+ * cold flow - callbackFlow, channelFlow
+ * hot flow - stateFlow, sharedFlow
+ */
+
 
 // 參考連結
 // [官方推荐 Flow 取代 LiveData, 有必要吗？](https://xie.infoq.cn/article/04e927ce35d5f9b66e5152c9e)
@@ -174,23 +196,3 @@ class DataUseCase {
 // [Coroutine 停看聽](https://ithelp.ithome.com.tw/users/20129264/ironman/3966)
 // [谁能取代Android的LiveData- StateFlow or SharedFlow](https://blog.csdn.net/eclipsexys/article/details/122227697)
 // [A safer way to collect flows from Android UIs](https://medium.com/androiddevelopers/a-safer-way-to-collect-flows-from-android-uis-23080b1f8bda)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
